9917695686243817f3a59fc94bd87279ff293c58
Revans31 <ivansrega@gmail.com>
2024-01-25 13:57:53 +0700

    pembuatan halaman login, register, book, peminjaman untuk peminjam

--- lib/app/modules/peminjaman/views/peminjaman_view.dart
+++ lib/app/modules/peminjaman/views/peminjaman_view.dart
@@ -1,22 +1,61 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../controllers/peminjaman_controller.dart';
 
 class PeminjamanView extends GetView<PeminjamanController> {
   const PeminjamanView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PeminjamanView'),
+        title: Text(
+          'Peminjaman View',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
+        backgroundColor: Colors.blue,
       ),
-      body: const Center(
-        child: Text(
-          'PeminjamanView is working',
-          style: TextStyle(fontSize: 20),
+      body: controller.obx(
+            (state) => ListView.separated(
+          itemCount: state!.length,
+          itemBuilder: (context, index) {
+            return Card(
+              elevation: 2,
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: ListTile(
+                leading: Icon(
+                  Icons.bookmarks_sharp,
+                  color: Colors.blue,
+                ),
+                title: Text(
+                  "${state[index].book?.judul}",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Nama User: ${state[index].user?.nama}",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    Text(
+                      "Tanggal Pinjam: ${state[index].tanggalPinjam}",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    Text(
+                      "Tanggal Kembali: ${state[index].tanggalKembali}",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider();
+          },
         ),
       ),
     );
